7c78fb9c6ecfcf1fb3e44910c9ff76b40fa17157
Letícia Gabrielle <130669115+leticia44@users.noreply.github.com>
2024-04-12 16:09:18 -0300

    le

--- conexao.php
+++ conexao.php
@@ -1,7 +1,6 @@
 <?php
     try {
-        // Substitua 'caminho/para/seu/banco/de/dados.db' pelo caminho real do seu arquivo de banco de dados SQLite
-        $pdo = new PDO('login.db');
+        $pdo = new PDO('sqlite:login.db');
     } catch (PDOException $e) {
         echo "Erro!: " . $e->getMessage();
         die();
